88075e7eae6a76b56fe8604e7470272a9dbfe67c
Daniel Garcia <github@mail.daniel.es>
2025-12-05 17:58:27 +0000

    backend: Enable eager execution of Celery tasks in DEBUG mode

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -502,6 +502,9 @@ CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TASK_SERIALIZER = "json"
 
+# Run celery tasks in the same shell as the backend in DEBUG mode
+CELERY_ALWAYS_EAGER = DEBUG
+
 # Configure Beat Periodic Tasks in the database
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
